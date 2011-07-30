eb595542c9d52f08495d3603718b2cff0e2c832b
Steen Larsen <steen666@gmail.com>
2011-07-30 18:40:23 +0200

    first

---
+++ src/fsm_abonnement/core.clj
@@ -0,0 +1,1 @@
+(ns fsm-abonnement.core)
---
+++ test/fsm_abonnement/test/core.clj
@@ -0,0 +1,6 @@
+(ns fsm-abonnement.test.core
+  (:use [fsm-abonnement.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
